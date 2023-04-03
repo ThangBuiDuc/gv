8d8a759751e861786c73eed190f56e6dea868bc8
ThangBuiDuc <thangbd@hpu.edu.vn>
2023-04-03 15:47:48 +0700

    test

--- api/fast-work.js
+++ api/fast-work.js
@@ -4,7 +4,7 @@ import os from 'os';
 
 export default async function Handler(req, res) {
     
-    const result = await fetch(`https://app.fastwork.vn/timesheets?tokenkey=e88652cb44dc424d2eedee30b151c075`,{
+    const result = await fetch(`https://api.fastwork.vn:6010/v1/timesheets?tokenkey=e88652cb44dc424d2eedee30b151c075`,{
         method:'POST',
         body:{
             "code":"Cb21@hpu",//mã đăng nhập trên FW
--- src/component/homePage/index.jsx
+++ src/component/homePage/index.jsx
@@ -1,26 +1,7 @@
-import { useEffect } from "react";
 import "../../App.css";
 
 export default function Index() {
-  useEffect(() => {
-    let callApi = async () => {
-      await fetch(
-        `https://api.fastwork.vn:6010/v1/timesheets?tokenkey=${import.meta.env.VITE_FAST_WORK}`,
-        {
-          method: "POST",
-          body: {
-            code: "Cb21@hpu", //mã đăng nhập trên FW
-            from: "2023-11-08", //Thời gian bắt đầu
-            to: "2023-11-08", //Thời gian kết thúc
-          },
-        }
-      ).then((res) => res.json())
-      .then(res => console.log(res))
-    };
-
-    callApi();
-  },[]);
-
+  
 
   return <div className="wrap">{/* <h2>Xin chào </h2> */}</div>;
 }
