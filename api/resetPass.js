07ff83b547c54ba41370df4da8907e07d0e0e0e8
ThangBuiDuc <thangbd@hpu.edu.vn>
2023-06-19 14:15:38 +0700

    fix env

--- api/resetPass.js
+++ api/resetPass.js
@@ -1,23 +1,23 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-  const { id, pass} = req.body;
+  const { id, pass } = req.body;
 
   const status = await fetch(`https://api.clerk.dev/v1/users/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.VITE_APP_CLERK_SECRET_KEY}`,
+      Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
     },
     body: JSON.stringify({
-        password: pass
+      password: pass,
     }),
-  })
-  .then(res => res.json())
-//   .catch(err => err.errors[0].message)
+  }).then((res) => res.json());
+  //   .catch(err => err.errors[0].message)
 
-//   console.log(status)
+  //   console.log(status)
 
-  if(await status.errors) return res.status(400).json({result: status.errors[0].message})
-  else return res.status(200).json({ result: "successful" })
+  if (await status.errors)
+    return res.status(400).json({ result: status.errors[0].message });
+  else return res.status(200).json({ result: "successful" });
 }
