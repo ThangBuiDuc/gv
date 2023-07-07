e93671f99727f77b27a73258295178772c44784d
ThangBuiDuc <thangbd@hpu.edu.vn>
2023-07-07 09:34:24 +0700

    hotfix1

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
